35c2387e814f8a0f64831dad5c08f4a0a50bec13
Noah Rollins <noahjrollins@gmail.com>
2022-11-01 23:23:11 -0500

    commit tree 7a15367e0cf754e751e354d36a224542ca6065de

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+    function getWin(record){
+        return (record.result === "W")
+    }
+    if (record.find(getWin)){
+    return record.find(getWin).year;
+    } 
+}
